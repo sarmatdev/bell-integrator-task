b49a9659c8e4dd9cbbb4715191ec91a3f4c7861a
Sarmat Khachirov <skhachirov12@gmail.com>
2021-01-18 11:50:34 +0300

    fix: proxy

--- src/api/httpClient.ts
+++ src/api/httpClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const httpClient = axios.create({
-  baseURL: 'https://thingproxy.freeboard.io/fetch/http://188.166.35.35:5000/api/v1',
+  baseURL: 'http://cors-anywhere.herokuapp.com/http://188.166.35.35:5000/api/v1',
   headers: {
     'Content-Type': 'application/json'
   }
