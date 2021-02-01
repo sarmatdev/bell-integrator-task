cb3e2e9838480c099c799b303013c17cbb68eea7
Sarmat Khachirov <skhachirov12@gmail.com>
2021-02-01 12:28:36 +0300

    fix: server url

--- src/api/httpClient.ts
+++ src/api/httpClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const httpClient = axios.create({
-  baseURL: 'http://157.245.12.176:5000/api/v1',
+  baseURL: 'https://hohagtrip.com/api/v1',
   headers: {
     'Content-Type': 'application/json'
   }
